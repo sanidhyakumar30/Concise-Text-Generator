9a1ffdcb9b9a5aa9a7d1d9fe749a42239df9ccc3
Sanidhya Kumar <sanidhya_kumar@dell.com>
2024-02-17 17:58:55 +0530

    Document Summary Generator

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+__version__= "0.0.0"
+REPO_NAME = "Concise-Text-Generator"
+AUTHOR_USER_NAME= "sanidhyakumar30"
+SRC_REPO= "conciseTextGenerator"
+AUTHOR_EMAIL= "kumarsanidhya@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
